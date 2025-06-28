bbd69a72625c4e72a683c38b7292716c2390204e
LeonidMehandzhijski <leonidmehandzijski@gmail.com>
2025-06-28 18:31:48 +0200

    Github Actions

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PortfolioSM/' : '/',
+  base: '/PortfolioSM/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
