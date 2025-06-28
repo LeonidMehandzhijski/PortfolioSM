a7e1e170dd953cc5a70b6e205f75f6ecfe3ac0aa
LeonidMehandzhijski <leonidmehandzijski@gmail.com>
2025-06-28 18:36:46 +0200

    Testing still

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,14 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths
+  base: '/PortfolioSM/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
       }
     }
   },
